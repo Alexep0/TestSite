66e5ff9aa90762e22cdf2ea9a309ac5343ff0b5a
Alexandr Popov <alexep130@yandex.ru>
2025-04-10 10:07:11 +0300

    10.04 fix

--- src/components/Stocks/Stocks.js
+++ src/components/Stocks/Stocks.js
@@ -2,7 +2,7 @@ import './Stocks.css';
 import CountdownTimer from './Timer/Timer';
 
 export default function Stocks() {
-    const targetDate = new Date('2025-04-10T00:00:00');
+    const targetDate = new Date('2025-04-11T00:00:00');
     return (
         <section className='stocks'>
             <p className='stocks_title'>"Не упустите шанс! Только до 10 Апреля — скидка 20% и подарок при оформлении заказа. Спешите, количество акционных товаров ограничено!"</p>
